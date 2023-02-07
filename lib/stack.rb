2f387c5517027709dc4b5fe8857e6e59d61fb997
Bernard Legaspi <blegaspi26@gmail.com>
2023-02-07 16:30:21 -0500

    commit tree f5ceeb371a4aa862e20c46451c1a84857dfc487c

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,44 @@
 # your code here
+
+
+class Stack 
+
+    # points to an empty array 
+    def initialize(limit = nil)
+        @Stack = []
+        @limit = limit 
+    end
+
+    #  add an element to the top of the stack 
+    def push(item)
+        # raise 'Stack Overflow' if full?
+        @stack.push(item)
+    end
+
+    # remove the element at the top of the stack
+    def pop
+        @stack.pop
+    end
+
+    # return the value of the element at the top of the stack without removing it
+    def peek
+        @stack.last
+    end
+
+    # def size 
+    #     @stack.length
+    # end
+
+    # def empty?
+    #     @stack.empty?
+    # end
+
+    # def full?
+    #     @limit && @stack.size == @limit
+    # end
+
+
+
+
+
+end 
